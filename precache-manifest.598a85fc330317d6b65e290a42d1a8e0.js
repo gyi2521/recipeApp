95528c412f74a11d07286419d9f016c272b387e3
gyi <ginay2521@gmail.com>
2019-01-04 00:00:17 -0500

    Updates

---
+++ precache-manifest.598a85fc330317d6b65e290a42d1a8e0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c87822401d21b69c9b12",
+    "url": "/recipeApp/static/js/runtime~main.c8782240.js"
+  },
+  {
+    "revision": "76f2ba62245169240f6f",
+    "url": "/recipeApp/static/js/main.76f2ba62.chunk.js"
+  },
+  {
+    "revision": "a44f0c0078e70b51841d",
+    "url": "/recipeApp/static/js/1.a44f0c00.chunk.js"
+  },
+  {
+    "revision": "76f2ba62245169240f6f",
+    "url": "/recipeApp/static/css/main.9a6602d5.chunk.css"
+  },
+  {
+    "revision": "ced1be419ea3546dca1a68224cc803a3",
+    "url": "/recipeApp/index.html"
+  }
+];
